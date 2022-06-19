364d1c5ae2ca42534655614a6a52df480f71956b
Kutub Gandhi <kksgandhi@gmail.com>
2022-06-18 22:13:29 -0400

    add pause functionality

---
+++ Godot/src/InGameUI.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func pause():
+	get_tree().paused = true
+	$MarginContainer/ResumeButton.show()
+	$"Darken Overlay".show()
+	$MarginContainer/PauseButton.hide()
+	
+func resume():
+	get_tree().paused = false
+	$MarginContainer/PauseButton.show()
+	$"Darken Overlay".hide()
+	$MarginContainer/ResumeButton.hide()
+
+func _on_PauseButton_pressed():
+	pause()
+
+func _on_ResumeButton_pressed():
+	resume()
